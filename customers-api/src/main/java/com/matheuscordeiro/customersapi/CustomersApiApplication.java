e24ebf1da591284550fd2cc22521c826e217893e
mbcordeiro <matheusdebarroscordeiro@gmail.com>
2023-03-14 21:40:24 -0300

    Configure eureka cliente customers-api

--- customers-api/src/main/java/com/matheuscordeiro/customersapi/CustomersApiApplication.java
+++ customers-api/src/main/java/com/matheuscordeiro/customersapi/CustomersApiApplication.java
@@ -2,8 +2,10 @@ package com.matheuscordeiro.customersapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class CustomersApiApplication {
 
 	public static void main(String[] args) {
