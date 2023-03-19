261aec461737a9cf380bc8714d62289a0020a03f
mbcordeiro <matheusdebarroscordeiro@gmail.com>
2023-03-19 18:16:16 -0300

    Add card customer entity

---
+++ cards-api/src/main/java/com/matheuscordeiro/cardsapi/domain/CustomerCard.java
@@ -0,0 +1,39 @@
+package com.matheuscordeiro.cardsapi.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.Hibernate;
+
+import java.math.BigDecimal;
+import java.util.Objects;
+
+@Entity
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+@RequiredArgsConstructor
+public class CustomerCard {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+    private String document;
+    @ManyToOne
+    @JoinColumn(name = "card_id")
+    private Card card;
+    private BigDecimal limit;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) return false;
+        CustomerCard that = (CustomerCard) o;
+        return id != null && Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return getClass().hashCode();
+    }
+}
