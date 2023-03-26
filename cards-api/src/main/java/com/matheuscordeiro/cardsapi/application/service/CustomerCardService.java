6a8fde4936ffdc04d2e2006773f21c8fa2f0d8c3
mbcordeiro <matheusdebarroscordeiro@gmail.com>
2023-03-26 13:05:47 -0300

    Customer card service

---
+++ cards-api/src/main/java/com/matheuscordeiro/cardsapi/application/service/CustomerCardService.java
@@ -0,0 +1,9 @@
+package com.matheuscordeiro.cardsapi.application.service;
+
+import com.matheuscordeiro.cardsapi.domain.CustomerCard;
+
+import java.util.List;
+
+public interface CustomerCardService {
+    List<CustomerCard> listCardsByDocument(String document);
+}
---
+++ cards-api/src/main/java/com/matheuscordeiro/cardsapi/application/service/impl/CustomerCardServiceImpl.java
@@ -0,0 +1,20 @@
+package com.matheuscordeiro.cardsapi.application.service.impl;
+
+import com.matheuscordeiro.cardsapi.application.service.CustomerCardService;
+import com.matheuscordeiro.cardsapi.domain.CustomerCard;
+import com.matheuscordeiro.cardsapi.infra.repository.CustomerCardRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class CustomerCardServiceImpl implements CustomerCardService {
+    private final CustomerCardRepository repository;
+
+    @Override
+    public List<CustomerCard> listCardsByDocument(String document) {
+        return repository.findByDocument(document);
+    }
+}
