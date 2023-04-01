72e536466ebf544c04b94bbec304b5011f87005e
mbcordeiro <matheusdebarroscordeiro@gmail.com>
2023-03-31 22:43:02 -0300

    Enable web security

---
+++ eureka-server/src/main/java/com/matheuscordeiro/eurekaserver/config/SecurityConfig.java
@@ -0,0 +1,13 @@
+package com.matheuscordeiro.eurekaserver.config;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable().authorizeRequests().anyRequest().authenticated().and().httpBasic();
+    }
+}
